437ef8d82d2c52735abeb093459a1f15849a7b19
Enkronan <robin@tempura.se>
2020-06-30 15:30:12 +0200

    dev
    added method for crossing out completed todos

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="todo-item">
-        <p>{{todo.title}}</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed
+        }
+    }
 }
 </script>
 
@@ -16,6 +24,9 @@ export default {
         background: #f4f4f4;
         padding: 10px;
         border-bottom: 1px #ccc dotted;
+    }
+    .is-complete{
+        text-decoration: line-through;
     }
 
 </style>
